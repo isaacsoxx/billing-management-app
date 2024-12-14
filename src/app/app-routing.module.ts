a4a79437b058a6e549dcb404042203bdb57a8e0a
isaacsoxx <isaacsolano06gmail.com>
2024-12-14 16:32:36 -0600

    configured initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersTableComponent } from './users/users-table/users-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full'
+  },
+  {
+    path: 'users',
+    component: UsersTableComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
